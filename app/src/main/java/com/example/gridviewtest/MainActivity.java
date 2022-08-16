41bcb12cabbcf529a18e1544bf6074aa2bda66ba
Habiba <habiba5543@live.com>
2022-08-16 13:33:40 +0200

    first uploaded project

---
+++ app/src/main/java/com/example/gridviewtest/MainActivity.java
@@ -0,0 +1,32 @@
+package com.example.gridviewtest;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageButton;
+
+public class MainActivity extends AppCompatActivity {
+
+    ImageButton but1;
+    ImageButton but2;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+//
+//        but1=(ImageButton)findViewById(R.id.imageButton);
+//        but2=(ImageButton)findViewById(R.id.imageButton2);
+//
+//        but1.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View view) {
+//                but1.setBackground(getApplicationContext().getDrawable(R.drawable.red_btn));
+//            }
+//        });
+
+
+    }
+}
